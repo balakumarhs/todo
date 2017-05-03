f16394cd43bcdc8af998cffff6f17277c7790d2c
balakumarhs <bs443@njit.edu>
2017-05-03 18:09:36 -0400

    add button

--- error/badinfo.php
+++ error/badinfo.php
@@ -1,7 +1,10 @@
+<div id="main">
+<h1 class="top">Error</h1>
+<p class="first_paragraph">User name or password is incorrect</p>
+</div>
+<form method = 'post' action='../index.php'>
+<input type="submit" value="Login Page"/>
+</form>
 
-	<div id="main">
-		<h1 class="top">Error</h1>
-			<p class="first_paragraph">User name or password is incorrect</p>
-				</div>
 										
 
