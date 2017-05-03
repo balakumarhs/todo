c2cba3249dbdc0762ef4a1214a3e015efbb668b0
balakumarhs <bs443@njit.edu>
2017-05-03 13:29:29 -0400

    add delete button

--- task_list/list.php
+++ task_list/list.php
@@ -15,12 +15,12 @@ echo "<br> <br>";
         <?php foreach($result as $res):?>
       <tr>
         <td> <?php echo $res['todo']; ?>  </td>
-	<td><form action="." method="post">
-	    <input type="hidden" name="action" value="delete_task">
+	<td><form action="index.php" method="post">
             <input type="hidden" name="user_id" value="<?php echo $res['id']; ?>">
 	    <input type="submit" value="Delete">
-	    </form>
-	</td>
+	    <input type="hidden" name='action' value="deletetask">
+            </form>
+        </td>
       </tr>  
 	<?php endforeach;?>
       
