d0cb376dad0a6f341b32583065088bc142a54c13
balakumarhs <bs443@njit.edu>
2017-05-07 13:14:36 -0400

    add footer.php

---
+++ view/footer.php
@@ -0,0 +1,23 @@
+<html>
+<style>
+.welcome{
+position:bottom;
+background:#4c75af;
+}
+p {
+margin-right: 5px;
+font-family: cursive;
+color: #fff;
+text-align: center;
+}
+
+</style>
+<body>
+<div class="welcome">
+<strong> <p> © 2017 To Do List.</p></strong>
+<strong> <p> Created By - Balakumar Shanmugam</p></strong>
+</div>
+</body>
+~
+~
+
